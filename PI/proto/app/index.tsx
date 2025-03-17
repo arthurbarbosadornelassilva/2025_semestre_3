56035810f72e3bec9eb9fb6fcdc4dac345e6ee11
Arthur Silva <xarthsilvax@gmail.com>
2025-03-17 16:40:50 -0300

    PRATICA - React Native

---
+++ PI/proto/app/index.tsx
@@ -0,0 +1,17 @@
+// O nome do arquivo principal deve ser sempre "index" ao utilizar o template de react-native com expo
+
+import { Text, View , StyleSheet} from "react-native"; 
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Oi Gustavo, pega no meu pau</Text>
+    </View>
+  );
+}
