37dc92a5be328f385b1bc36ab3c029808b9065d5
Arthur Silva <xarthsilvax@gmail.com>
2025-03-24 22:33:05 -0300

    ESTUDOS P1 - Paradigmas

--- PI/proto/app/index.tsx
+++ PI/proto/app/index.tsx
@@ -1,11 +1,21 @@
 // O nome do arquivo principal deve ser sempre "index" ao utilizar o template de react-native com expo
 
-import { Text, View , StyleSheet} from "react-native"; 
+import { Text, View , StyleSheet, Image} from "react-native"; 
+import Slider from '@react-native-community/slider'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Memória de Elefante</Text>
+      <Image
+        source={require("../assets/images/CADEADO.png")}
+        style={styles.logo}
+      />
+      <Text>Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+        />
+      </View>
     </View>
   );
 }
@@ -16,5 +26,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#F3F3FF',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  logo: {
+    marginBottom: 30
+  },
+  area: {
+    marginTop: 20,
+    marginBottom: 20,
+    width: "80%",
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    padding: 20
   }
 })
