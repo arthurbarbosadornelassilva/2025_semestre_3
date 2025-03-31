9b510e64ae71664b893d1c854b7d1f762d37462d
Arthur Silva <xarthsilvax@gmail.com>
2025-03-31 20:54:17 -0300

    Instanciando métodos para teste (pt.2)

--- ESTUDO_PROVAS/ALGORITMOS/ListaEstudoNossoVetor/NossoVetor.java
+++ ESTUDO_PROVAS/ALGORITMOS/ListaEstudoNossoVetor/NossoVetor.java
@@ -1,30 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class NossoVetor {
-    private String vetorStrings[];
+
+    private int vetorInt[];
     private int ocupacoes;
 
-    public NossoVetor(int tamanho){
-        this.vetorStrings = new String[tamanho];
+    public NossoVetor(int tamanho) {
+        this.vetorInt = new int[tamanho];
         this.ocupacoes = 0;
     }
+
     // ****MÉTODO 'estaCheio' CONFERE SE O VETOR ESTÁ CHEIO****
     public boolean estaCheio() {
-        return this.ocupacoes == vetorStrings.length;
+        return this.ocupacoes == vetorInt.length;
     }
 
     // 1. Inserir um elemento em uma determinada posição
-    public void insereElemento(String elemento, int i) {
-        if (estaCheio()){
+    public void insereElemento(int elemento, int i) {
+        if (estaCheio()) {
             System.out.println("Vetor cheio!");
         }
-        this.vetorStrings[i] = elemento;
+        this.vetorInt[i] = elemento;
         ocupacoes++;
     }
 
+    // **utilizei BUBBLE SORT para ordenar os vetores e comparar os elementos semelhantes**
+    public void bubbleSort() {
+        for (int i = 1; i < vetorInt.length - 1; i++) {
+            for (int j = 0; j < vetorInt.length - i; j++) {
+                if (vetorInt[j] > vetorInt[j + 1]) {
+                    int aux = vetorInt[j];
+                    vetorInt[j] = vetorInt[j + 1];
+                    vetorInt[j + 1] = aux;
+                }
+            }
+        }
+    }
+    
     // 2. Verificar quantas vezes um elemento aparece no vetor
+    
 
     // 3. Remover um elemento de uma determinada posição
 
     // 4. Remover a primeira ocorrência de um determinado elemento
 
     // 5. Remover todas as ocorrências de um determinado elemento
+
+    @Override
+    public String toString() {
+        return Arrays.toString(vetorInt);
+    }
 }
--- ESTUDO_PROVAS/ALGORITMOS/ListaEstudoNossoVetor/TesteNossoVetor.java
+++ ESTUDO_PROVAS/ALGORITMOS/ListaEstudoNossoVetor/TesteNossoVetor.java
@@ -1,6 +1,14 @@
 public class TesteNossoVetor {
     public static void main(String[] args) {
-        
-        
+        NossoVetor vetor = new NossoVetor(10);
+
+        vetor.insereElemento(1, 0);
+        vetor.insereElemento(4, 1);
+        vetor.insereElemento(2,2);
+        vetor.insereElemento(4, 3);
+        vetor.insereElemento(5, 4);
+
+        System.out.println(vetor.toString());
+        vetor.bubbleSort();
     }
 }
