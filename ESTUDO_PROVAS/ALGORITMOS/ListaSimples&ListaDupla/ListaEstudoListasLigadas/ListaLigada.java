d3296fe08cab57292d14aa3c59c1563a69d708f2
Arthur Silva <xarthsilvax@gmail.com>
2025-04-17 19:48:42 -0300

    PRATICA - Questões para trabalhar listas ligadas simples e duplas

---
+++ ESTUDO_PROVAS/ALGORITMOS/ListaSimples&ListaDupla/ListaEstudoListasLigadas/ListaLigada.java
@@ -0,0 +1,21 @@
+public class ListaLigada {
+    
+    // 1. Inserir um elemento em uma determinada posição.
+    // 2. Verificar quantas vezes um elemento aparece na lista.
+    // 3. Devolver a posição da primeira ocorrência de um determinado elemento.
+    // 4. Remover um elemento de uma determinada posição.
+    // 5. Remover a primeira ocorrência de um determinado elemento.
+    // 6. Remover todas as ocorrências de um determinado elemento.
+    // 7. Criar uma nova instância da classe, com as posições de ocorrência de um
+    // determinado elemento.
+    // 8. Devolver o tamanho atual da lista de valores (quantos elementos ela tem).
+    // 9. Esvaziar a lista.
+    // 10. Devolver uma cópia da lista.
+    // 11. Receber listas de entrada e mesclá-las numa única lista (considere as listas não
+    // ordenadas).
+    // 12. Somar os elementos da lista.
+    // 13. Encontrar o maior elemento armazenado na lista.
+    // 14. Encontrar a posição do maior elemento armazenado.
+    // 15. Encontrar o menor elemento armazenado e colocá-lo na posição 1 (fazer a troca).
+
+}
