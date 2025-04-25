bb3e03d44d3dff6213949a4f81d4aad4eb80db57
Arthur Silva <xarthsilvax@gmail.com>
2025-04-25 12:59:40 -0300

    Aula 25/04 - Algoritmos

--- algoritmos_t1s2/arvore_binaria/ArvoreBinaria.java
+++ algoritmos_t1s2/arvore_binaria/ArvoreBinaria.java
@@ -1,6 +1,49 @@
 public class ArvoreBinaria {
     private No raiz;
+    //construtor padrão
+    public boolean arvoreVazia() {
+        return raiz == null;
+    }
+    public void insere (int info) {
+        No novo = new No(info);
 
+        if (arvoreVazia()){
+            raiz = novo;
+        } else {
+            insereRec(novo, raiz);   // insereRec() -> função recursiva para inserir elementos na árvore
+        }
+    }
+    public void insereRec (No novo, No atual) {
+        if (novo.getInfo() > atual.getInfo()) {    // insere elementos maiores que info à direita
+            if (atual.getDireita() == null)
+                atual.setDireita(novo);
+            else
+                insereRec(novo, atual.getDireita());
+        } else {
+            if (atual.getEsquerda() == null)       // insere elementos menores que info à esquerda
+                atual.setEsquerda(novo);
+            else
+                insereRec(novo, atual.getEsquerda());
+        }
+    }
+    // usar o percurso em ordem simétrica para construit o toString
+    @Override
+    public String toString () {
+        if (arvoreVazia()) return "arvore vazia";
+        return toStringRec(raiz);
+    }
+    private String toStringRec(No atual) {
+        String s = "";
+        if (atual.getEsquerda() != null)
+            s = s + toStringRec(atual.getEsquerda());
+        
+            s = s + atual + " ";
+
+        if (atual.getDireita() != null)
+            s = s + toStringRec(atual.getDireita());
+
+        return s;
+    }
 }
 class  No {
     private int info;
@@ -21,5 +64,11 @@ class  No {
     public void setDireita(No direita) {
         this.direita = direita;
     }
-    
+    public int getInfo() {
+        return info;
+    }
+    @Override
+    public String toString() {
+        return info + "";
+    }
 }
